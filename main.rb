74688c255123313bd952e73b732e38db9e9f42be
monokemonoke <kawabataryuhei@gmail.com>
2022-10-15 18:06:35 +0900

    lex

--- main.rb
+++ main.rb
@@ -1,8 +1,29 @@
+def parser(src)
+    src.sub!('(', ' ( ')
+    src.sub!(')', ' ) ')
+    return src.split(' ')
+end
+
+
+# testsrcs = [
+#     '(+ 1 2)',
+#     '(- 1 2)',
+#     '(* 1 2)',
+#     '(/ 1 2)',
+#     '(+ 10 2)',
+#     '(+ -10 2)',
+#     '(+ -10 252)',
+# ]
+
+# for testsrc in testsrcs do
+#     p parser testsrc
+# end
+
 while true do
     print "> "
     text = gets
     if text == nil
         break
     end
-    print text
+    puts "#{parser text}"
 end
