b1366d0465ec6afc178d86c0fac0f5d34e675000
monokemonoke <kawabataryuhei@gmail.com>
2022-10-15 18:25:23 +0900

    eval

--- main.rb
+++ main.rb
@@ -1,23 +1,63 @@
-def parser(src)
+def parse(src)
     src.sub!('(', ' ( ')
     src.sub!(')', ' ) ')
     return src.split(' ')
 end
 
+def test_parse()
+    testsrcs = [
+        '(+ 1 2)',
+        '(- 1 2)',
+        '(* 1 2)',
+        '(/ 1 2)',
+        '(+ 10 2)',
+        '(+ -10 2)',
+        '(+ -10 252)',
+    ]
 
-# testsrcs = [
-#     '(+ 1 2)',
-#     '(- 1 2)',
-#     '(* 1 2)',
-#     '(/ 1 2)',
-#     '(+ 10 2)',
-#     '(+ -10 2)',
-#     '(+ -10 252)',
-# ]
+    for testsrc in testsrcs do
+        p parse testsrc
+    end
+end
 
-# for testsrc in testsrcs do
-#     p parser testsrc
-# end
+def eval(ast)
+    ast.shift
+    ast.pop
+    func = ast.shift
+
+    case func
+    when "+"
+        default = ast.shift.to_i
+        return ast.inject(default) {|sum, i| sum + i.to_i}
+    when "-"
+        default = ast.shift.to_i
+        return ast.inject(default) {|res, i| res - i.to_i}
+    when "*"
+        default = ast.shift.to_i
+        return ast.inject(default) {|mul, i| mul * i.to_i}
+    when "/"
+        default = ast.shift.to_i
+        return ast.inject(default) {|res, i| res / i.to_i}
+    else
+        p "err: func is #{func}"
+    end
+end
+
+def test_eval()
+    testsrcs = [
+        '(+ 1 2)',
+        '(- 1 2)',
+        '(* 1 2)',
+        '(/ 1 2)',
+        '(+ 10 2)',
+        '(+ -10 2)',
+        '(+ -10 252)',
+    ]
+
+    for testsrc in testsrcs do
+        p eval parse testsrc
+    end
+end
 
 while true do
     print "> "
@@ -25,5 +65,5 @@ while true do
     if text == nil
         break
     end
-    puts "#{parser text}"
+    puts "#{eval parse text}"
 end
